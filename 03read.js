fc6dc16ab74d3464d272bb8392f64a1332b5a554
raghu693 <sonia.nagar16@gmail.com>
2024-04-12 19:38:05 +0530

    5

--- 03read.js
+++ 03read.js
@@ -1,2 +1,4 @@
 db.inventory.find() // - Fetch all documents
 db.inventory.find({qty:85}) // - Fetch the selected documents
+
+db.inventory.find({tags:{$in:["red", "gray"]}}) // - find the documents which matches the condition
