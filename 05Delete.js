a37199ca1d34e6cacf8c5ac149703eb8dd945df3
raghu693 <sonia.nagar16@gmail.com>
2024-04-13 09:33:29 +0530

    9

--- 05Delete.js
+++ 05Delete.js
@@ -4,13 +4,30 @@ db.inventory.deleteOne({}) // - To Delete one documents
 
 // We can also give Conditions Init
 
+// For DeleteOne
+// db.inventory.deleteOne({condition}) 
+// EX -
+db.inventory.deleteOne({item:"journal"}) // - Deletes the first document with item journal
+
 // For DeleteMany
 
 // db.inventory.deleteMany({condition}) 
 // EX - 
 db.inventory.deleteMany({item:"mousepad"}) // - Deletes all the documents with item mousepad
 
-// For DeleteOne
-// db.inventory.deleteOne({condition}) 
+// There is one more method called remove
+
+db.inventory.remove({}) // - Removes all the documents
+
+// We can give condition init also
+
+// db.inventory.remove({condition})
 // EX -
-db.inventory.deleteOne({item:"journal"}) // - Deletes the first document with item journal
+db.inventory.remove({item:"mat"}) // - Deletes all the documents with item mat
+
+// So, What is the difference between ewmove and deleteMany
+
+// The Major Difference is the return values
+
+// In deleteMany we get the return value of how many documents are deleted (the Number)
+// In remove we get the return value of the documents which are deleted (the document itself get returned)
